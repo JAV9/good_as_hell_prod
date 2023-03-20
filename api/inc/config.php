b6ed4c1d4cbc1cea5bacda1812d1a5851c4068ed
JAV9 <javi6311@gmail.com>
2023-03-20 22:10:16 +0100

    datos de bd a prod

--- api/inc/config.php
+++ api/inc/config.php
@@ -2,13 +2,13 @@
 
 // Database
 
-define("DB_HOST", "localhost");
-//define("DB_HOST", "vm20.db.swarm.test");
+//define("DB_HOST", "localhost");
+define("DB_HOST", "vm20.db.swarm.test");
 
 define("DB_USERNAME", "root");
 
-define("DB_PASSWORD", "root");
-//define("DB_PASSWORD", "Cei7shohy8Oohie");
+//define("DB_PASSWORD", "root");
+define("DB_PASSWORD", "Cei7shohy8Oohie");
 
 define("DB_DATABASE_NAME", "good_as_hell");
 
